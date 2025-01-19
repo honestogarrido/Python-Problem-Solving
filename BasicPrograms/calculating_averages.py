8d4b7a714270c6c0f833535c23cf5626c8d16ae7
Honesto Garrido <honestogarrido@outlook.com>
2025-01-19 12:49:43 +0000

    Calculating averages

---
+++ BasicPrograms/calculating_averages.py
@@ -0,0 +1,29 @@
+"""Write a program to take marks of 5 subjects as an input from the user and calculate their average. 
+
+Sample Run: 
+
+Enter the marks for Subject 1: 85 
+
+Enter the marks for Subject 2: 90 
+
+Enter the marks for Subject 3: 78 
+
+Enter the marks for Subject 4: 92 
+
+Enter the marks for Subject 5: 88 
+
+
+The average marks of the 5 subjects is: 86.6  """ 
+
+
+1=int(input("Entermarksforsubject1")) 
+
+2=int(input("Entermarksforsubject2")) 
+
+3=int(input("Entermarksforsubject3")) 
+
+4=int(input("Entermarksforsubject4")) 
+
+5=int(input("Entermarksforsubject5")) 
+
+average=(1+2+3+4+5)/5 
