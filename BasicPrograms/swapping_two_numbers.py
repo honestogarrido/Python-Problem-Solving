c24049eeff517c271f6fca2681fa7009e3280364
Honesto Garrido <honestogarrido@outlook.com>
2025-01-19 20:02:13 +0000

    swapping two numbers

---
+++ BasicPrograms/swapping_two_numbers.py
@@ -0,0 +1,13 @@
+"""Write a program to swap two numbers using a third variable.""" 
+
+a= int(input("Enter number for a ")) 
+b= int(input("Enter number for b "))  
+print(a,b)  
+
+c= a  
+
+a= b  
+
+b= c 
+print(a,b) 
+ 
