05d47c35991c2c1b973a382ef720abbf55775c60
Honesto Garrido <honestogarrido@outlook.com>
2025-01-19 12:43:38 +0000

    program for finding the area and perimeter of a rectangle

---
+++ BasicPrograms/rectangle.py
@@ -0,0 +1,13 @@
+"""Write a program to find the area and perimeter of the rectangle (using user input) """
+
+ 
+length = 10 
+width = 20  
+area = length * width 
+print("Area of rectangle is", area)  
+
+
+length = 10 
+width = 20  
+perimeter = 2 (length + width) 
+print("Area of rectangle is", area)
