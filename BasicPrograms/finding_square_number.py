067edb3a7652bbbba3634655eaab5006f55e11af
Honesto Garrido <honestogarrido@outlook.com>
2025-01-19 19:59:22 +0000

    finding the square number

---
+++ BasicPrograms/finding_square_number.py
@@ -0,0 +1,13 @@
+"""Write a program to find the square of a number taken as user input. 
+
+Example: 
+
+Enter your number: 8 
+
+Square of 8 is 64  """ 
+
+num= int(input("Enter your number "))  
+print(num)  
+
+square= num**2  
+print(square) 
