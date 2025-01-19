376e8a81c2b6ab4179c17de1d4f610f642566a00
Honesto Garrido <honestogarrido@outlook.com>
2025-01-19 20:02:53 +0000

    calculating the volume of a cone

---
+++ BasicPrograms/volume_of_a_cone.py
@@ -0,0 +1,10 @@
+"""Write a program to input Principal amount, rate of interest and time taken to calculate its simple interest. Formula: Simple Interest = (Principal amt x Rate x Time)/100 
+
+Write a program to calculate volume of Cone""" 
+
+r= int(input("Enter the radius "))  
+h= int(input("Enter h "))  
+
+volume= 3.14*r**2 
+
+print (volume)  
