434bc3fae1a41d72b36129404a713a2284b46e4b
Honesto Garrido <honestogarrido@outlook.com>
2025-01-23 14:26:26 +0000

    gardening question

---
+++ BasicPrograms/gardening_question.py
@@ -0,0 +1,17 @@
+"""Question: Write a Python program to calculate the total water needed for a garden. Prompt the user to enter the number of plants in the garden and the water needed per plant (in liters). Then calculate and display the total water needed. 
+
+ 
+
+Sample Run 
+
+Welcome to the Gardening Planner! 
+
+Please enter the number of plants in your garden: 10 
+
+Please enter the water needed per plant (in liters): 0.5 """ 
+
+
+print(" Welcome to the Gardening Planner!")  
+plants= int(input("Enter the number of plants in your garden ")) 
+water= plants/2  
+print("You need", water, "litres of water") 
